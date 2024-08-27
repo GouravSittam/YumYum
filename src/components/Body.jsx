c2a1be4f4dfcba647cd61f70e308bc7d1c0bdd7f
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-28 00:11:44 +0530

    Hooks

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,19 @@
 import RestaurantCards from "./RestaurantCard";
 import resList from "../Utils/mockData";
 
-
 const Body = () => {
   return (
     <div className="body">
-      <div className="Search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("Button Clicked");
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCards key={restaurant.info.id} resData={restaurant} />
