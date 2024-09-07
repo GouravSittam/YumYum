8fd5ffb8634f641509020f94853cbbe2dc55b542
Gourav Chaudhary <gouravsittam@gmail.com>
2024-09-07 17:49:52 +0530

    useEffect Hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,24 +1,40 @@
 import RestaurantCards from "./RestaurantCard";
 import resList from "../Utils/mockData";
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   //State variable -super powerful variable
-  
+  const [ListOfRestaurants, setListofRestraunt] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+  };
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("Button Clicked");
+            const filteredList = ListOfRestaurants.filter(
+              (res) => res.info.avgRating > 3
+            );
+            setListofRestraunt(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {ListOfRestaurants.map((restaurant) => (
           <RestaurantCards key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
