6a800a0ff81e8aec9bc415933ca6e08bf4e5c637
Gourav Chaudhary <gouravsittam@gmail.com>
2024-09-21 23:23:41 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import { createBrowserRouter,  RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
@@ -21,6 +22,10 @@ const appRouter = createBrowserRouter([
   {
     path: "/about",
     element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+
+const Contact = () => {
+  return <div>
+    <h1>Contact</h1>
+    <h2>Yum Yum</h2>
+    <h2>+91 9876543210</h2>
+    <h2>EMAIL</h2>
+    <h2>Facebook: <a href="https://www.facebook.com/Yum-Yum-Restaurant-and-Cafe-10991667880184/">@yummyumcafe</a></h2>
+  </div>
+};
+
+export default Contact;
