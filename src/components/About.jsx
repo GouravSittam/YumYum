2de10dcd7c92233308a24fd1d8f3c177d4561167
Gourav Chaudhary <gouravsittam@gmail.com>
2024-09-30 21:03:46 +0530

    Component DidMount

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,43 @@
 // import User from "./User";
+// import { render } from "react-dom";
 import UserClass from "./UserClass";
-const About=()=>{
-    return(
-        <div>
-        <h1>About</h1>
-        <h2>This is a about page</h2>
-        {/* <User name={"Gourav Chaudhary (function)"} /> */}
+import { Component } from "react";
 
-        <UserClass name={"Gourav chaudhary (class)"}/>
-        </div>
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
+
+  componentDidMount(){
+    console.log("parent componet Didmount")
+  }
+
+  render() {
+    console.log("parent Render")
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is an about page</h2>
+        {/* <User name={"Gourav Chaudhary (function)"} /> */}
+        <UserClass name={"Gourav Chaudhary (class)"} />
+      </div>
     );
-};
+  }
+}
+
 export default About;
+
+
+// const About=()=>{
+//     return(
+//         <div>
+//         <h1>About</h1>
+//         <h2>This is a about page</h2>
+//         {/* <User name={"Gourav Chaudhary (function)"} /> */}
+
+//         <UserClass name={"Gourav chaudhary (class)"}/>
+//         </div>
+//     );
+// };
+// export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -4,33 +4,35 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state={
-        count:0,
-       
-    }
-
+    this.state = {
+      count: 0,
+    };
   }
 
-componentDidMount(){
-    console.log("child componentDidMount")
-}
+  componentDidMount() {
+    console.log("child componentDidMount");
+
+    //here we call the api
+  }
 
   render() {
     const { name } = this.props;
-    const {count}=this.state;
+    const { count } = this.state;
 
-    console.log("child render")
+    console.log("child render");
     return (
       <div className="user-card">
         <h2>count: {count}</h2>
-        <button onClick={()=>{
+        <button
+          onClick={() => {
             //never state variable directly
             this.setState({
-                count: this.state.count+1,  
-            })
-        }}>Count Increase</button>
-
-
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count Increase
+        </button>
 
         <h2>name: {name}</h2>
         <h2>Location: Delhi</h2>
