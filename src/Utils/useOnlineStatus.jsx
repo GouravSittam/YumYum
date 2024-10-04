edbccd5323cb944f6aa2c8de9aa5097c46b1f087
Gourav Chaudhary <gouravsittam@gmail.com>
2024-10-05 00:32:41 +0530

    Custom Hooks

--- src/Utils/useOnlineStatus.jsx
+++ src/Utils/useOnlineStatus.jsx
@@ -1,24 +1,19 @@
 import { useEffect, useState } from "react";
 
-
-
-const useOnlineStatus=()=>{
-    const[onlineStatus, setOnlineStatus]=useState(true);
+const useOnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(true);
+  // try if online
+  useEffect(() => {
+    window.addEventListener("offline", () => {
+      setOnlineStatus(false);
+    });
     // try if online
+    window.addEventListener("online", () => {
+      setOnlineStatus(true);
+    });
+  }, []);
 
-    useEffect(()=>{
-        window.addEventListener("offline",()=>{
-            setOnlineStatus(false);
-        })
-        window.addEventListener("online",()=>{
-            setOnlineStatus(true);
-        })
-
-    },[]);
-
-
-
-    //BOOLENA VALUE 
-    return onlineStatus;
-}
+  //BOOLENA VALUE
+  return onlineStatus;
+};
 export default useOnlineStatus;
