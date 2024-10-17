3e1583bcd65142299a0da6bc3497be0bac260717
Gourav Chaudhary <gouravsittam@gmail.com>
2024-10-17 20:46:50 +0530

    Redux

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ItemList from "./ItemList";
+import { clearCart } from "../Utils/CartSlices";
 
 const Cart = () => {
+  const cartItems = useSelector((store) => store.cart.items);
+  console.log(cartItems);
+
+  const dispatch = useDispatch();
+
+  const handelClearCart = () => {
+    dispatch(clearCart());
+  };
+
   return (
     <div className="text-center m-4 p-4">
-      <h1 className="text-2xl font-bold">Le lo jo lena hai</h1>
+      <h1 className="text-2xl font-bold">Cart</h1>
+      <button
+        className="p-2 m-2 bg-black text-white rounded-l"
+        onClick={handelClearCart}
+      >
+        Clear Cart
+      </button>
+      {cartItems.length === 0 && <h1>No Items</h1>}
+      <div className="w-6/12 m-auto">
+        <ItemList items={cartItems} />
+      </div>
     </div>
   );
 };
